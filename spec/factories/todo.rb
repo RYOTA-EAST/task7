95e2f67d59ea5406f6b99dbaad645372170c89a3
Ryota Higashi <ryo19870130@outlook.jp>
2022-03-12 15:01:40 +0900

    モデル単体テストtodo新規登録

--- spec/factories/todo.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :todo do
-    name { Faker::Lorem.characters(number:20) }
-    description { Faker::Lorem.characters(number:20) }
-  end
-end
---
+++ spec/factories/todos.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :todo do
+    name { Faker::Lorem.characters(number: 20) }
+    description { Faker::Lorem.characters(number: 20) }
+  end
+end
