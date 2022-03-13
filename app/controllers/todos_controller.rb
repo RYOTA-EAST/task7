1e9976070d1c3e91e8ee0a060990fbbe6da250ed
Ryota Higashi <ryo19870130@outlook.jp>
2022-03-13 21:45:46 +0900

    作成したタスクを上に表示

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,7 @@
 class TodosController < ApplicationController
   before_action :set_todo, only: %i(show edit update destroy)
   def index
-    @todos = Todo.all
+    @todos = Todo.all.order(created_at: :desc)
   end
 
   def show
