66accba4c361870e6a1d2cd849d7a4d9f1f9a1e0
Ryota Higashi <ryo19870130@outlook.jp>
2022-04-02 23:35:56 +0900

    エラーメッセージ日本語化

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,8 +2,8 @@ class Todo < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   validates :name, presence: true
-  validates :status_id, numericality: { greater_than_or_equal_to: 2 }
-  validates :priority_id, numericality: { greater_than_or_equal_to: 2 }
+  validates :status_id, numericality: { greater_than_or_equal_to: 2, message: 'を選択してください' }
+  validates :priority_id, numericality: { greater_than_or_equal_to: 2, message: 'を選択してください' }
   validates :deadline, presence: true
 
   belongs_to :user
